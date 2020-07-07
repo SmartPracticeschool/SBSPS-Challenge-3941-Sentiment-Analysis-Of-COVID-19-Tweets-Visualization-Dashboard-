7cd6f5eb57dce5180c28aded7a40029b8be67222
Prajakta Tamte <prajaktatamte@gmail.com>
2020-07-07 21:43:41 +0530

    L

--- ibmhc2020/settings.py
+++ ibmhc2020/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'staticfiles')
+    os.path.join(BASE_DIR,'template')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'statics')
 
